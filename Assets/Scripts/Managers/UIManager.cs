f84afce770941defc21122f65a91621863ad4a3e
DaniilHryhulevich <dan.h@dcjudgement.com>
2024-04-30 19:28:26 +0400

    add base logic for project

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,81 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Screens;
+
+namespace Managers
+{
+    public class UIManager : Singleton<UIManager>
+    {
+        [SerializeField] private List<BaseScreen> screensPrefab = new List<BaseScreen>();
+        [SerializeField] private List<BaseScreen> screens = new List<BaseScreen>();
+
+        [SerializeField] private Camera camera;
+
+        public bool IsSetup = false;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            SetupScreen();
+        }
+
+        private void Start()
+        {
+            //OpenScreen<StartScreen>();
+
+            IsSetup = true;
+        }
+
+        private void SetupScreen()
+        {
+            for (int i = 0; i < screensPrefab.Count; i++)
+            {
+                var addScreen = Instantiate(screensPrefab[i], gameObject.transform);
+                addScreen.SetCamera(camera);
+                addScreen.CloseScreen();
+                screens.Add(addScreen);
+            }
+        }
+
+        public void OpenScreen<T>() where T : BaseScreen
+        {
+            T screen = GetScreen<T>();
+
+            if (screen != null)
+            {
+                screen.OpenScreen();
+            }
+            else
+            {
+                UnityEngine.Debug.LogError($"Screen of type {typeof(T)} is not registered.");
+            }
+        }
+
+        public void CloseScreen<T>() where T : BaseScreen
+        {
+            T screen = GetScreen<T>();
+
+            if (screen != null)
+            {
+                screen.CloseScreen();
+            }
+            else
+            {
+                UnityEngine.Debug.LogError($"Screen of type {typeof(T)} is not registered.");
+            }
+        }
+
+        public T GetScreen<T>() where T : BaseScreen
+        {
+            foreach (var screen in screens)
+            {
+                if (screen is T)
+                {
+                    return (T)screen;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Screens/BaseScreen.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Managers;
+
+namespace Screens
+{
+    public class BaseScreen : MonoBehaviour
+    {
+        public Canvas Canvas;
+        public RectTransform Content;
+
+        public bool IsOpenSound;
+
+        public virtual void OpenScreen()
+        {
+            //if (UIManager.Instance.IsSetup && IsOpenSound)
+            //    AudioManager.Instance.ButtonClickSound();
+
+            if (Content)
+                Content.gameObject.SetActive(true);
+        }
+
+        public virtual void CloseScreen()
+        {
+            //if (UIManager.Instance.IsSetup && IsOpenSound)
+            //    AudioManager.Instance.ButtonClickSound();
+
+            if (Canvas && Content)
+                Content.gameObject.SetActive(false);
+        }
+
+        public void SetCamera(Camera camera)
+        {
+            if (Canvas)
+            {
+                Canvas.worldCamera = camera;
+            }
+
+        }
+    }
+}
